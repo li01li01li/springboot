1fba173847db227e6cbd397d5edae2d5077b34db
1808281062@qq.com <1808281062@qq.com>
2019-04-18 09:30:15 +0800

    last

---
+++ spring-boot-websocket/src/main/java/com/spring/boot/websocket/WebsocketApplication.java
@@ -0,0 +1,14 @@
+package com.spring.boot.websocket;
+
+import com.spring4all.swagger.EnableSwagger2Doc;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@EnableSwagger2Doc
+@SpringBootApplication
+public class WebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocketApplication.class, args);
+    }
+
+}
